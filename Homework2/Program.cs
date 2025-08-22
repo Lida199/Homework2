0795c2e8a0567ba8c4f213fee4b4fbc547bc433c
Lida Shubitidze <lidashubitidze@credo.ge>
2025-08-22 19:02:56 +0400

    Initial State For Console App

---
+++ Homework2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Lida Shubitidze");
+Console.BackgroundColor = ConsoleColor.Blue;
+Console.ForegroundColor = ConsoleColor.Magenta;
+
+Console.WriteLine("Please input your text here:");
+string inputFromUser = Console.ReadLine();
+Console.WriteLine($"You input was: {inputFromUser}");
